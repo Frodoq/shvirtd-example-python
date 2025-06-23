c0d7affd9811bd266fe412c4289525ebb8c5aaf0
vagrant <vagrant@server1.netology>
2025-06-23 12:27:16 +0000

    add_proxy4netology

--- custom_middleware.py
+++ custom_middleware.py
@@ -1,8 +1,13 @@
 from fastapi import Request
 
 async def add_real_ip_header(request: Request, call_next):
-    if not request.headers.get('x-real-ip'):
-        request.headers.__dict__['_list'].append(
-            (b'x-real-ip', b'127.0.0.1')
-        )
+    real_ip = (
+        request.headers.get('X-Forwarded-For', '').split(',')[0].strip() or
+        request.headers.get('X-Real-IP') or
+        str(request.client.host)
+    
+    request.headers.__dict__['_list'] = [
+        (k, v) for k, v in request.headers.items() if k.lower() != 'x-real-ip'
+    ] + [('x-real-ip', real_ip.encode())]
+    
     return await call_next(request)
